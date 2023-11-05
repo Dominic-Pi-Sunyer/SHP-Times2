b8137c63465694ad1f9b2b88b005500bdf2ae972
Fires170 <149727367+Dominic-Pi-Sunyer@users.noreply.github.com>
2023-11-04 17:37:02 -0700

    removed user requst funtion for html and js

--- index.js
+++ index.js
@@ -116,33 +116,3 @@ if (savedDarkMode === "true") {
     document.body.classList.add("dark-mode");
 }
 
-let saveFile = () => {
-
-    // Get the data from each element on the form.
-    const email = document.getElementById('txtEmail');
-    const msg = document.getElementById('msg');
-
-    // This variable stores all the data.
-    let data =
-        'Email: ' + email.value + ' \r\n ' +
-        'Message: ' + msg.value;
-
-    // Convert the text to BLOB.
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'formData.txt';	   // The file to save the data.
-
-    let newLink = document.createElement("a");
-    newLink.download = sFileName;
-
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
-
-    newLink.click();
-}
-document.cookie = "mode=darkOn"
